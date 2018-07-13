11f3021ae90a6258b8bf35daa54324d9f2a7ce5b
Vitor Rodrigo Vezani <vitor.vezani@gmail.com>
2018-07-12 22:40:11 -0300

    Added all for Book entity

---
+++ grails-app/domain/br/com/vvezani/Book.groovy
@@ -0,0 +1,7 @@
+package br.com.vvezani
+
+class Book {
+
+    static constraints = {
+    }
+}
---
+++ grails-app/services/br/com/vvezani/BookService.groovy
@@ -0,0 +1,18 @@
+package br.com.vvezani
+
+import grails.gorm.services.Service
+
+@Service(Book)
+interface BookService {
+
+    Book get(Serializable id)
+
+    List<Book> list(Map args)
+
+    Long count()
+
+    void delete(Serializable id)
+
+    Book save(Book book)
+
+}
